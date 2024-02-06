61253a971f97d6c4fc75054de6c37fefcce52417
dinesh ubrani <dineshubrani1109@gmail.com>
2024-02-06 18:36:30 +0530

    changes done

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "https://farm2fork.onrender.com//api/v2";
+export const server = "https://farm2fork.onrender.com/api/v2";
 export const backend_url = "https://farm2fork.onrender.com/";
